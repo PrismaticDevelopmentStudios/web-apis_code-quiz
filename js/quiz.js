63d18a3cc3c44b20c7c5011d653dde140ed0f4e6
Matthew Brignola <matthewbrignola@ymail.com>
2021-11-13 22:04:59 -0700

    cannot get to deduct time if wrong. otherwise ok

--- js/quiz.js
+++ js/quiz.js
@@ -9,7 +9,12 @@ const quizEl = $('#quiz');
 const li = $('<LI>');
 const p = $('<A>')
 const div = $('<DIV>');
-const button = $('<button class="answwers">Select</button>');
+const exitBtn = $('#exitBtn');
+const submitBtn = $('#submit');
+const scoreBoard = $('#scores');
+const scoreBtn = $('#scoreBtn');
+
+submitBtn.addClass('submit');
 
 let current = 0;
 
@@ -29,12 +34,12 @@ const bB = "b. Backend";
 const bC = "c. Front end";
 const bD = "d. Full stack";
 // 3
-const q3 = '3. What is the difference between a "var" and a "const" in JavaScript?';
+const q3 = `3. What does the non null assertion '!' operator do?`;
 // Answers
-const cA = 'a. Var can hold a variety of values';
-const cB = 'b. Const is easily changeable';
-const cC = `c. Const's value is immmutable`;
-const cD = 'd. Var is an immmutable value';
+const cA = 'a. It adds emphasis to a string';
+const cB = 'b. Reverses the order of an array';
+const cC = `c. It denotes the opposite case of the specified value`;
+const cD = 'd. It remove the need for key value pairs ';
 // 4
 const q4 = '4. What does the acronym "API" stand for?';;
 // Answers
@@ -100,91 +105,104 @@ const answers = [{
         answer1: bA,
         answer2: bB,
         answer3: bC,
-        answer4: bD
+        answer4: bD,
+        correct: bA
     },
     {
         answer1: cA,
         answer2: cB,
         answer3: cC,
-        answer4: cD
+        answer4: cD,
+        correct: cC
     },
     {
         answer1: dA,
         answer2: dB,
         answer3: dC,
-        answer4: dD
+        answer4: dD,
+        correct: dD
     },
     {
         answer1: eA,
         answer2: eB,
         answer3: eC,
-        answer4: eD
+        answer4: eD,
+        correct: eA
     },
     {
         answer1: fA,
         answer2: fB,
         answer3: fC,
-        answer4: fD
+        answer4: fD,
+        correct: fD
     },
     {
         answer1: gA,
         answer2: gB,
         answer3: gC,
-        answer4: gD
+        answer4: gD,
+        correct: gC
     },
     {
         answer1: hA,
         answer2: hB,
         answer3: hC,
-        answer4: hD
+        answer4: hD,
+        correct: hB
     },
     {
         answer1: iA,
         answer2: iB,
         answer3: iC,
-        answer4: iD
+        answer4: iD,
+        correct: iC
     },
     {
         answer1: jA,
         answer2: jB,
         answer3: jC,
-        answer4: jD
+        answer4: jD,
+        correct: jB
     },
 ];
-
-// Select Answer
-function select() {
-    console.log('clicked');
-}
 
 // Hides Back & Next Buttons on Page Load
 next.hide();
 back.hide();
+exitBtn.hide();
+submitBtn.hide();
 // Start Quiz
 let start = false;
+let time = 900;
+let initials
 
 function startQuiz() {
+    initials = prompt('Eenter your initials');
     start = true;
     next.show();
     back.show();
+    exitBtn.show();
+    submitBtn.show();
     title.hide();
     startBox.hide();
+    scoreBoard.hide();
     // Timer Func
-    let time = 900;
     let intervalID = setInterval(myCallback, 1000, time);
     timer.text('15:00');
 
     function myCallback() {
         time--;
         if (time === 0) {
-            clearInterval(intervalID)
+            clearInterval(intervalID);
+            finished();
         }
         let min = Math.floor(time / 60);
-        let sec = time - min * 60;
+        sec = time - min * 60;
         if (sec < 10) {
             sec = '0' + sec
         }
         timer.text(min + ':' + sec);
+
         if (time === 0) {
             timer.text('Time Up').css({
                 'margin-left': '50%',
@@ -194,6 +212,7 @@ function startQuiz() {
             back.hide();
             quizEl.hide();
         }
+        return time;
     }
     for (let i = 0; i < questions.length; i++) {
         quizEl.append(li).html(
@@ -204,6 +223,21 @@ function startQuiz() {
         ).addClass('li');
     }
 } // END Start Quiz END
+
+
+function finished() {
+    let affirm = confirm(`Are you sure you want to exit and submit your current score?`)
+    if (affirm) {
+        console.log(correct, 'submit');
+        if (score !== undefined) {
+            localStorage.setItem(date, score);
+        }
+    } else {
+        return
+    }
+    sessionStorage.clear();
+    location.reload();
+}
 
 // Hide Elements on Load
 // Next 
@@ -240,16 +274,142 @@ startBtn.click(function() {
     startQuiz();
 });
 
+let date;
+let score;
+
+let correct = 0;
+let incorrect = 0;
 $(document).on('click', '.answers', function(e) {
-    console.log(e.target);
-    $('.answers').css({
-        'border': 'none'
-    });
-    $(this).css({
-        'border': '2px solid yellow'
-    });
-    console.log($(this).text());
-    if ($(this).text() === answers[current].correct) {
-        console.log('correct');
+    if (current === 0) {
+        if (e.target.innerHTML === answers[0].answer2) {
+            sessionStorage.setItem('1', 'Correct');
+            correct++;
+        } else {
+            sessionStorage.setItem('1', 'Incorrect');
+            incorrect++;
+        }
     }
+    if (current === 1) {
+        if (e.target.innerHTML === answers[1].answer1) {
+            sessionStorage.setItem('2', 'Correct');
+            correct++;
+        } else {
+            sessionStorage.setItem('2', 'Incorrect');
+            incorrect++;
+        }
+    }
+    if (current === 2) {
+        if (e.target.innerHTML === answers[2].answer3) {
+            sessionStorage.setItem('3', 'Correct');
+            correct++;
+        } else {
+            sessionStorage.setItem('3', 'Incorrect');
+            incorrect++;
+        }
+    }
+    if (current === 3) {
+        if (e.target.innerHTML === answers[3].answer4) {
+            sessionStorage.setItem('4', 'Correct');
+            correct++;
+        } else {
+            sessionStorage.setItem('4', 'Incorrect');
+            incorrect++;
+        }
+    }
+    if (current === 4) {
+        if (e.target.innerHTML === answers[4].answer1) {
+            sessionStorage.setItem('5', 'Correct');
+            correct++;
+        } else {
+            sessionStorage.setItem('5', 'Incorrect');
+            incorrect++;
+        }
+    }
+    if (current === 5) {
+        if (e.target.innerHTML === answers[5].answer4) {
+            sessionStorage.setItem('6', 'Correct');
+            correct++;
+        } else {
+            sessionStorage.setItem('6', 'Incorrect');
+            incorrect++;
+        }
+    }
+    if (current === 6) {
+        if (e.target.innerHTML === answers[6].answer3) {
+            sessionStorage.setItem('7', 'Correct');
+            correct++;
+        } else {
+            sessionStorage.setItem('7', 'Incorrect');
+            incorrect++;
+        }
+    }
+    if (current === 7) {
+        if (e.target.innerHTML === answers[7].answer2) {
+            sessionStorage.setItem('8', 'Correct');
+            correct++;
+        } else {
+            sessionStorage.setItem('8', 'Incorrect');
+            incorrect++;
+        }
+    }
+    if (current === 8) {
+        if (e.target.innerHTML === answers[8].answer3) {
+            sessionStorage.setItem('9', 'Correct');
+            correct++;
+        } else {
+            sessionStorage.setItem('9', 'Incorrect');
+            incorrect++;
+        }
+    }
+    if (current === 9) {
+        if (e.target.innerHTML === answers[9].answer2) {
+            correct++;
+        } else {
+            sessionStorage.setItem('10', 'Incorrect');
+            incorrect++;
+        }
+    }
+    date = new Date;
+    score = correct + '/10 ' + initials.valueOf();
+});
+
+function submit() {
+    let affirm = confirm(`Are you sure you're ready to finish?`);
+    if (affirm) {
+        console.log(correct, 'submit');
+        if (score !== undefined) {
+            localStorage.setItem(date, score);
+        }
+    } else {
+        return
+    }
+}
+
+
+submitBtn.on('click', function() {
+    submit()
+});
+
+$(document).on('click', '#exit', function() {
+    finished();
+});
+let scores = [];
+
+function getScores() {
+
+    scores = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while (i--) {
+        scores.push(localStorage.getItem(keys[i]));
+    }
+    scores = scores.join(', ');
+    return scores;
+}
+
+scoreBtn.on('click', function() {
+    getScores();
+    scoreBoard.text(scores);
+    scoreBoard.addClass('scoreBoard')
 });
